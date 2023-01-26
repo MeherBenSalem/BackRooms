42a083c0da7a4a732dd2a00323b3d8f5f974e7ff
MeherBenSalem <mahourabensalem@gmail.com>
2023-01-26 22:36:51 +0100

    day_10

--- Assets/Resources/MyScripts/Props/Keypad.cs
+++ Assets/Resources/MyScripts/Props/Keypad.cs
@@ -8,20 +8,36 @@ public class Keypad : MonoBehaviour
     public string correctCode = "1234";
     private string inputCode = "";
     public UnityEvent onCodeEntered;
-    public UnityEvent onWrongCodeEntered;
+    public AudioClip buttonSound;
+    public AudioClip successSound;
+    public AudioClip failSound;
+    AudioSource player;
+
+    void Start(){
+        player = GetComponent<AudioSource>();
+    }
 
     public void EnterCode(string code)
     {
+        PlayAudio(buttonSound);
         inputCode += code;
-        if (inputCode.Length > 4)
-        {
-            onWrongCodeEntered.Invoke();
-            inputCode = "";
-        }
-        else if (inputCode == correctCode)
-        {
+        if(inputCode.Length < 4)
+            return;
+        if (inputCode == correctCode)
+        {       
+            PlayAudio(successSound);
             onCodeEntered.Invoke();
             inputCode = "";
         }
+        else
+        {
+            PlayAudio(failSound);
+            inputCode = "";
+        }
+    }
+
+    public void PlayAudio(AudioClip clip){
+        player.clip = clip;
+        player.Play();
     }
 }
